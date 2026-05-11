fa98fd5cd4186b687b688faf08fd128b6a685ef4
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-11 10:43:25 -0500

    Add Nivel class header file

---
+++ logica/Nivel.h
@@ -0,0 +1,36 @@
+#ifndef NIVEL_H
+#define NIVEL_H
+
+#include <vector>
+
+#include "Jugador.h"
+#include "Enemigo.h"
+
+class Nivel
+{
+protected:
+
+    Jugador* jugador;
+
+    std::vector<Enemigo*> enemigos;
+
+    bool terminado;
+
+public:
+
+    Nivel();
+
+    virtual ~Nivel();
+
+    virtual void actualizar() = 0;
+
+    virtual void cargarNivel() = 0;
+
+    bool estaTerminado() const;
+
+    Jugador* getJugador() const;
+
+    std::vector<Enemigo*>& getEnemigos();
+};
+
+#endif
