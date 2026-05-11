eff027fbc13a0fed22a851a62c1475fcd9691811
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-11 10:43:46 -0500

    Implement Nivel class with constructor and destructor

---
+++ logica/Nivel.cpp
@@ -0,0 +1,35 @@
+#include "Nivel.h"
+
+Nivel::Nivel()
+{
+    jugador = nullptr;
+
+    terminado = false;
+}
+
+Nivel::~Nivel()
+{
+    delete jugador;
+
+    for(Enemigo* enemigo : enemigos)
+    {
+        delete enemigo;
+    }
+
+    enemigos.clear();
+}
+
+bool Nivel::estaTerminado() const
+{
+    return terminado;
+}
+
+Jugador* Nivel::getJugador() const
+{
+    return jugador;
+}
+
+std::vector<Enemigo*>& Nivel::getEnemigos()
+{
+    return enemigos;
+}
