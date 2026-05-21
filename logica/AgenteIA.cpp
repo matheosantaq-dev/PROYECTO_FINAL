77726179b96c59eb556ad8feb91dedeeb3eb2138
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-21 12:33:13 -0500

    Implement AgenteIA class for AI behavior

---
+++ logica/AgenteIA.cpp
@@ -0,0 +1,44 @@
+#include "AgenteIA.h"
+
+AgenteIA::AgenteIA(Jugador* jugador,
+                   JefeFinal* jefe,
+                   int dificultad)
+{
+    this->jugador = jugador;
+
+    this->jefe = jefe;
+
+    this->dificultad = dificultad;
+}
+
+void AgenteIA::actualizarIA()
+{
+    int velocidadIA = 2;
+
+    // Dificultad afecta IA
+    if(dificultad == 0)
+    {
+        velocidadIA = 1;
+    }
+
+    if(dificultad == 1)
+    {
+        velocidadIA = 3;
+    }
+
+    if(dificultad == 2)
+    {
+        velocidadIA = 5;
+    }
+
+    // Boss sigue al jugador
+    if(jugador->getX() < jefe->getX())
+    {
+        jefe->setX(jefe->getX() - velocidadIA);
+    }
+
+    if(jugador->getX() > jefe->getX())
+    {
+        jefe->setX(jefe->getX() + velocidadIA);
+    }
+}
