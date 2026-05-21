d030b89130426d1bc00983d90ed31533c344f407
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-21 12:55:27 -0500

    Implement main menu with player name and difficulty selector

---
+++ QT/MenuPrincipal.cpp
@@ -0,0 +1,37 @@
+#include "MenuPrincipal.h"
+
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QComboBox>
+#include <QLineEdit>
+
+MenuPrincipal::MenuPrincipal()
+{
+    QVBoxLayout* layout =
+        new QVBoxLayout(this);
+
+    nombreJugador =
+        new QLineEdit();
+
+    nombreJugador->setPlaceholderText(
+        "Nombre jugador"
+    );
+
+    selectorDificultad =
+        new QComboBox();
+
+    selectorDificultad->addItem("Facil");
+
+    selectorDificultad->addItem("Intermedio");
+
+    selectorDificultad->addItem("Dificil");
+
+    botonJugar =
+        new QPushButton("JUGAR");
+
+    layout->addWidget(nombreJugador);
+
+    layout->addWidget(selectorDificultad);
+
+    layout->addWidget(botonJugar);
+}
