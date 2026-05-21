c3215442f313fbde2fbbcf3056ef20fa9a059b8b
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-21 12:37:51 -0500

    Update NivelBoss.cpp

--- logica/NivelBoss.cpp
+++ logica/NivelBoss.cpp
@@ -4,12 +4,18 @@ NivelBoss::NivelBoss()
 {
     jefe = nullptr;
 
+    ia = nullptr;
+
+    modoFuria = false;
+
     cargarNivel();
 }
 
 NivelBoss::~NivelBoss()
 {
     delete jefe;
+
+    delete ia;
 
     for(Dardo* dardo : dardos)
     {
@@ -22,40 +28,91 @@ NivelBoss::~NivelBoss()
     }
 
     dardos.clear();
+
     balones.clear();
 }
 
 void NivelBoss::cargarNivel()
 {
-    
+    // Crear jugador
     jugador = new Jugador(400, 700);
 
-
+    // Crear jefe
     jefe = new JefeFinal(400, 100);
+
+    // Crear IA
+    ia = new AgenteIA(
+        jugador,
+        jefe,
+        1
+    );
 }
 
 void NivelBoss::actualizar()
 {
+    // Actualizar tiempo
+    temporizador.actualizar();
+
+   
     jugador->actualizar();
 
+  
     jefe->actualizar();
 
-    // Actualizar dardos
+    
+    ia->actualizarIA();
+
+    // Activar modo furia
+    if(temporizador.getTiempoActual() >= 30
+        && !modoFuria)
+    {
+        modoFuria = true;
+
+        jefe->setVelocidadX(12);
+
+        jefe->setVida(
+            jefe->getVida() + 200
+        );
+    }
+
     for(Dardo* dardo : dardos)
     {
         dardo->actualizar();
     }
 
-    // Actualizar balones
+
     for(Balon* balon : balones)
     {
         balon->actualizar();
     }
 
-    // Ataques automáticos del jefe
+ 
     if(jefe->getTiempoAtaque() % 120 == 0)
     {
         lanzarDardo();
+    }
+
+    // Colisiones balones vs jefe
+    for(Balon* balon : balones)
+    {
+        if(balon->estaActivo())
+        {
+            float dx =
+                balon->getX() - jefe->getX();
+
+            float dy =
+                balon->getY() - jefe->getY();
+
+            if(dx < 50 && dx > -50 &&
+               dy < 50 && dy > -50)
+            {
+                balon->desactivar();
+
+                jefe->recibirDanio(20);
+
+                puntaje.agregarPuntos(100);
+            }
+        }
     }
 
     // Verificar derrota del jefe
@@ -87,4 +144,19 @@ void NivelBoss::lanzarDardo()
     );
 
     dardos.push_back(nuevoDardo);
+}
+
+int NivelBoss::getPuntaje() const
+{
+    return puntaje.getPuntos();
+}
+
+int NivelBoss::getTiempo() const
+{
+    return temporizador.getTiempoActual();
+}
+
+JefeFinal* NivelBoss::getJefe() const
+{
+    return jefe;
 }
