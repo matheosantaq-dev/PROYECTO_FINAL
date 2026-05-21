8a15626e41dff67bd8fbaf8a5ccf22ce53fb4093
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-21 12:41:01 -0500

    Add EscenaJuego class for game scene management

---
+++ QT/EscenaJuego.h
@@ -0,0 +1,38 @@
+#ifndef ESCENAJUEGO_H
+#define ESCENAJUEGO_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QTimer>
+#include <QKeyEvent>
+
+#include "../logica/NivelBoss.h"
+
+class EscenaJuego : public QGraphicsView
+{
+    Q_OBJECT
+
+private:
+
+    QGraphicsScene* escena;
+
+    NivelBoss* nivel;
+
+    QTimer* timer;
+
+    QGraphicsPixmapItem* spriteJugador;
+
+    QGraphicsPixmapItem* spriteBoss;
+
+public:
+
+    EscenaJuego(QWidget* parent = nullptr);
+
+    void actualizarJuego();
+
+protected:
+
+    void keyPressEvent(QKeyEvent* event) override;
+};
+
+#endif
