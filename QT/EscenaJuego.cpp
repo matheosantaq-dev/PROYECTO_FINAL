44d8bf589cbc554e59d958d6e224d7ac09e1948c
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-25 17:33:15 -0500

    Update EscenaJuego.cpp

--- QT/EscenaJuego.cpp
+++ QT/EscenaJuego.cpp
@@ -1,19 +1,21 @@
-// =========================
-// EscenaJuego.cpp
-// =========================
-
 #include "EscenaJuego.h"
 
 #include <QPixmap>
 #include <QBrush>
 #include <QString>
+#include <QFont>
 
 EscenaJuego::EscenaJuego(QWidget* parent)
     : QGraphicsView(parent)
 {
     escena = new QGraphicsScene(this);
 
-    escena->setSceneRect(0,0,1200,800);
+    escena->setSceneRect(
+        0,
+        0,
+        1200,
+        800
+        );
 
     setScene(escena);
 
@@ -24,90 +26,243 @@ EscenaJuego::EscenaJuego(QWidget* parent)
     nivelActual =
         gameManager->getNivelActual();
 
-    setBackgroundBrush(
-        QBrush(Qt::darkGreen)
-    );
+    frameJugador=0;
 
-    spriteJugador =
+    contadorAnimacion=0;
+
+    mirandoDerecha=true;
+
+    // =====================
+    // FONDO
+    // =====================
+
+    QPixmap fondo(
+        ":/Recursos/fondos/fondo_selva.png"
+        );
+
+    fondo=
+        fondo.scaled(
+            1200,
+            800,
+            Qt::IgnoreAspectRatio,
+            Qt::SmoothTransformation
+            );
+
+    setBackgroundBrush(
+        QBrush(
+            fondo
+            )
+        );
+
+    // =====================
+    // SPRITESHEET JUGADOR
+    // =====================
+
+    spriteSheetJugador=
+        QPixmap(
+            ":/Recursos/fondos/spray jugador (1).png"
+            );
+
+    QPixmap frameInicial=
+        spriteSheetJugador.copy(
+            70,
+            500,
+            180,
+            220
+            );
+
+    spriteJugador=
         escena->addPixmap(
-            QPixmap(80,80)
+            frameInicial
+            );
+
+    spriteJugador->setScale(
+        0.8
         );
 
     spriteJugador->setPos(
-        nivelActual->getJugador()->getX(),
-        nivelActual->getJugador()->getY()
-    );
+        nivelActual->getJugador()->getX()-70,
+        nivelActual->getJugador()->getY()-170
+        );
 
-    spriteBoss =
+    // =====================
+    // BOSS
+    // =====================
+
+    spriteBoss=
         escena->addPixmap(
             QPixmap(120,120)
-        );
+            );
 
     spriteBoss->hide();
 
-    textoVida =
-        escena->addText("");
+
+    // =====================
+    // PROYECTILES
+    // =====================
+
+    spriteSheetProyectiles=
+        QPixmap(
+            ":/Recursos/fondos/PROYECTILES.png"
+            );
+
+    // BALON
+
+    QPixmap frameBalon=
+        spriteSheetProyectiles.copy(
+            0,
+            0,
+            64,
+            64
+            );
+
+    spriteBalon=
+        escena->addPixmap(
+            frameBalon
+            );
+
+    spriteBalon->hide();
+
+    spriteBalon->setScale(
+        1.0
+        );
+
+    // DARDO
+
+    QPixmap frameDardo=
+        spriteSheetProyectiles.copy(
+            0,
+            320,
+            64,
+            64
+            );
+
+    spriteDardo=
+        escena->addPixmap(
+            frameDardo
+            );
+
+    spriteDardo->hide();
+
+    spriteDardo->setScale(
+        1.0
+        );
+
+    // =====================
+    // HUD
+    // =====================
+
+    QFont fuenteHUD;
+
+    fuenteHUD.setPointSize(
+        18
+        );
+
+    fuenteHUD.setBold(
+        true
+        );
+
+    textoVida=
+        escena->addText(
+            "Vida:100"
+            );
+
+    textoVida->setFont(
+        fuenteHUD
+        );
 
     textoVida->setDefaultTextColor(
         Qt::white
-    );
+        );
 
-    textoVida->setPos(20,20);
+    textoVida->setPos(
+        30,
+        20
+        );
 
-    textoPuntos =
-        escena->addText("");
+    textoPuntos=
+        escena->addText(
+            "Nivel 1"
+            );
+
+    textoPuntos->setFont(
+        fuenteHUD
+        );
 
     textoPuntos->setDefaultTextColor(
         Qt::yellow
-    );
+        );
 
-    textoPuntos->setPos(20,60);
+    textoPuntos->setPos(
+        30,
+        60
+        );
 
-    textoTiempo =
-        escena->addText("");
+    textoTiempo=
+        escena->addText(
+            "WASD + SPACE"
+            );
+
+    textoTiempo->setFont(
+        fuenteHUD
+        );
 
     textoTiempo->setDefaultTextColor(
         Qt::red
-    );
+        );
 
-    textoTiempo->setPos(20,100);
+    textoTiempo->setPos(
+        30,
+        100
+        );
 
-    barraVidaFondo =
+    // =====================
+    // VIDA BOSS
+    // =====================
+
+    barraVidaFondo=
         escena->addRect(
             800,
             20,
             300,
             30
-        );
+            );
 
     barraVidaFondo->setBrush(
         Qt::black
-    );
+        );
 
-    barraVida =
+    barraVida=
         escena->addRect(
             800,
             20,
             300,
             30
-        );
+            );
 
     barraVida->setBrush(
         Qt::red
-    );
+        );
 
     barraVida->hide();
 
     barraVidaFondo->hide();
 
-    timer = new QTimer(this);
+    timer=
+        new QTimer(
+            this
+            );
 
-    connect(timer,
-            &QTimer::timeout,
-            this,
-            &EscenaJuego::actualizarJuego);
+    connect(
+        timer,
+        &QTimer::timeout,
+        this,
+        &EscenaJuego::actualizarJuego
+        );
 
-    timer->start(16);
+    timer->start(
+        16
+        );
 }
 
 EscenaJuego::~EscenaJuego()
@@ -115,90 +270,95 @@ EscenaJuego::~EscenaJuego()
     delete gameManager;
 }
 
+void EscenaJuego::actualizarAnimacionJugador()
+{
+    QPixmap frame=
+        spriteSheetJugador.copy(
+            70,
+            500,
+            180,
+            220
+            );
+
+    spriteJugador->setPixmap(
+        frame
+        );
+}
+
 void EscenaJuego::actualizarJuego()
 {
     gameManager->actualizar();
 
-    nivelActual =
+    nivelActual=
         gameManager->getNivelActual();
 
-    if(gameManager->estaTerminado())
-    {
-        timer->stop();
-
-        textoTiempo->setPlainText(
-            "GANASTE"
-        );
-
-        return;
-    }
+    actualizarAnimacionJugador();
 
     spriteJugador->setPos(
-        nivelActual->getJugador()->getX(),
-        nivelActual->getJugador()->getY()
-    );
-
-    NivelBoss* bossNivel =
-        dynamic_cast<NivelBoss*>(
-            nivelActual
+        nivelActual->getJugador()->getX()-70,
+        nivelActual->getJugador()->getY()-170
         );
 
-    if(bossNivel != nullptr)
+    textoVida->setPlainText(
+        "Vida: "
+        +
+        QString::number(
+            nivelActual
+                ->getJugador()
+                ->getVida()
+            )
+        );
+
+    NivelBoss* bossNivel=
+        dynamic_cast<NivelBoss*>(
+            nivelActual
+            );
+
+    if(bossNivel!=nullptr)
     {
+        if(!bossNivel->getBalones().empty())
+        {
+            Balon* b=
+                bossNivel->getBalones()[0];
+
+            spriteBalon->show();
+
+            spriteBalon->setPos(
+                b->getX(),
+                b->getY()
+                );
+        }
+
+        else
+        {
+            spriteBalon->hide();
+        }
+
+        if(!bossNivel->getDardos().empty())
+        {
+            Dardo* d=
+                bossNivel->getDardos()[0];
+
+            spriteDardo->show();
+
+            spriteDardo->setPos(
+                d->getX(),
+                d->getY()
+                );
+        }
+
+        else
+        {
+            spriteDardo->hide();
+        }
+
         spriteBoss->show();
 
         barraVida->show();
 
         barraVidaFondo->show();
-
-        spriteBoss->setPos(
-            bossNivel->getJefe()->getX(),
-            bossNivel->getJefe()->getY()
-        );
-
-        textoVida->setPlainText(
-            "Vida Boss: "
-            + QString::number(
-                bossNivel->getJefe()->getVida()
-            )
-        );
-
-        textoPuntos->setPlainText(
-            "Puntos: "
-            + QString::number(
-                bossNivel->getPuntaje()
-            )
-        );
-
-        textoTiempo->setPlainText(
-            "Tiempo: "
-            + QString::number(
-                bossNivel->getTiempo()
-            )
-        );
-
-        float vida =
-            bossNivel->getJefe()->getVida();
-
-        float ancho =
-            (vida / 400.0f) * 300;
-
-        barraVida->setRect(
-            800,
-            20,
-            ancho,
-            30
-        );
-
-        if(bossNivel->getTiempo() >= 30)
-        {
-            spriteBoss->setScale(1.5);
-
-            barraVida->setBrush(
-                Qt::darkRed
-            );
-        }
     }
+
     else
     {
         spriteBoss->hide();
@@ -210,44 +370,49 @@ void EscenaJuego::actualizarJuego()
 }
 
 void EscenaJuego::keyPressEvent(
-        QKeyEvent* event)
+    QKeyEvent* event
+    )
 {
-    if(nivelActual == nullptr)
+    if(event->key()==Qt::Key_A)
     {
-        return;
+        nivelActual
+            ->getJugador()
+            ->moverIzquierda();
     }
 
-    if(event->key() == Qt::Key_A)
+    if(event->key()==Qt::Key_D)
     {
-        nivelActual->getJugador()
-                ->moverIzquierda();
+        nivelActual
+            ->getJugador()
+            ->moverDerecha();
     }
 
-    if(event->key() == Qt::Key_D)
+    if(event->key()==Qt::Key_W)
     {
-        nivelActual->getJugador()
-                ->moverDerecha();
+        nivelActual
+            ->getJugador()
+            ->saltar();
     }
 
-    if(event->key() == Qt::Key_W)
+    if(event->key()==Qt::Key_Space)
     {
-        nivelActual->getJugador()
-                ->saltar();
-    }
-
-    if(event->key() == Qt::Key_Space)
-    {
-        nivelActual->lanzarBalon();
+        nivelActual
+            ->lanzarBalon();
     }
 }
 
 void EscenaJuego::keyReleaseEvent(
-        QKeyEvent* event)
+    QKeyEvent* event
+    )
 {
-    if(event->key() == Qt::Key_A
-        || event->key() == Qt::Key_D)
+    if(
+        event->key()==Qt::Key_A
+        ||
+        event->key()==Qt::Key_D
+        )
     {
-        nivelActual->getJugador()
-                ->detener();
+        nivelActual
+            ->getJugador()
+            ->detener();
     }
 }
