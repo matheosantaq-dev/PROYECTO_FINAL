2fab029f73f8723a5e1c0c3f27d7ed26168916f9
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:15:49 -0500

    Update Dardo.cpp

--- logica/Dardo.cpp
+++ logica/Dardo.cpp
@@ -1,18 +1,16 @@
 #include "Dardo.h"
 
-Dardo::Dardo(float x, float y)
-    : Proyectil(x, y, 0, 6)
+Dardo::Dardo(float x, float y, int dificultad)
+    : Proyectil(x, y, 0.0f, 4.0f + dificultad * 2.0f, 10)
 {
-
+    ancho = 8;
+    alto  = 24;
 }
 
 void Dardo::actualizar()
 {
+    // Ligera aceleracion descendente (no puramente rectilineo)
+    velocidadY += 0.08f;
     mover();
-
-   
-    if(y > 900)
-    {
-        activo = false;
-    }
+    if (y > 950) activo = false;
 }
