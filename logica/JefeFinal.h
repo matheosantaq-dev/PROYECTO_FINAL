a355d295ca669b192606b930a7362eb14e83b6ec
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:06:54 -0500

    Refactor JefeFinal class with new attributes and methods
    Added new member variables and updated getter methods.

--- logica/JefeFinal.h
+++ logica/JefeFinal.h
@@ -2,31 +2,34 @@
 #define JEFEFINAL_H
 
 #include "Enemigo.h"
+#include <cmath>
 
-class JefeFinal : public Enemigo
-{
+class JefeFinal : public Enemigo {
 private:
-
+    int vida;
+    int vidaMaxima; 
     int fase;
-
-    float velocidadAtaque;
-
     int tiempoAtaque;
+    float xCentro;
+    float amplitud;
+    float omega;
+    int ticksGlobales;
 
 public:
-
     JefeFinal(float x, float y);
 
     void actualizar() override;
-
     void cambiarFase();
 
-    int getFase() const;
 
-    int getTiempoAtaque() const;
+    int getVida() const { return vida; }
+    int getVidaMaxima() const { return vidaMaxima; }
+    int getFase() const { return fase; }
+    int getTiempoAtaque() const { return tiempoAtaque; }
+    float getXCentro() const { return xCentro; }
 
-
-    void setVida(int nuevaVida);
+    void setVida(int v);
+    void setDificultad(int dif);
 };
 
-#endif
+#endif 
