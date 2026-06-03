6f5dfd28f29a1c0d57a6edff7a3faeafd0199173
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:03:51 -0500

    Update Entidad.h

--- logica/Entidad.h
+++ logica/Entidad.h
@@ -1,59 +1,42 @@
 #ifndef ENTIDAD_H
 #define ENTIDAD_H
 
+#include <stdexcept>
+
+// Clase abstracta base para todas las entidades del juego.
+// Implementa posicion, velocidad, hitbox y colision real.
 class Entidad
 {
 protected:
-
-    // Posición
     float x;
     float y;
-
-    // Velocidad
     float velocidadX;
     float velocidadY;
-
-    // Tamaño hitbox
     int ancho;
     int alto;
 
 public:
-
-    Entidad(float x, float y);
-
+    Entidad(float x, float y, int ancho = 64, int alto = 64);
     virtual ~Entidad();
 
-
     virtual void mover() = 0;
-
     virtual void actualizar() = 0;
 
-    
-   
+
+    bool colisionaCon(const Entidad* otra) const;
 
     float getX() const;
-
     float getY() const;
-
     float getVelocidadX() const;
-
     float getVelocidadY() const;
-
     int getAncho() const;
-
     int getAlto() const;
 
- 
     void setX(float x);
-
     void setY(float y);
-
     void setVelocidadX(float velocidad);
-
     void setVelocidadY(float velocidad);
-
     void setAncho(int ancho);
-
     void setAlto(int alto);
 };
 
