67cb0e53c1fcdf16f77eb65d9dc48d268e7e7524
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:27:07 -0500

    Change detectarColision to accept const pointers

--- logica/MotorFisico.h
+++ logica/MotorFisico.h
@@ -3,13 +3,12 @@
 
 #include "Entidad.h"
 
+// Motor de colisiones AABB.
+// Usa las dimensiones reales (ancho/alto) de cada entidad.
 class MotorFisico
 {
 public:
-
-    static bool detectarColision(
-            Entidad* a,
-            Entidad* b);
+    static bool detectarColision(const Entidad* a, const Entidad* b);
 };
 
 #endif
