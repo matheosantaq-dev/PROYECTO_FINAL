ab7ff4d0ea33a008c93e6379919dd1d45c232e66
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:10:38 -0500

    Refactor Proyectil constructor and damage attribute

--- logica/Proyectil.h
+++ logica/Proyectil.h
@@ -3,31 +3,23 @@
 
 #include "Entidad.h"
 
+
 class Proyectil : public Entidad
 {
 protected:
-
-    int danio;
-
+    int  danio;
     bool activo;
 
 public:
-
-    Proyectil(float x, float y,
-               float velocidadX,
-               float velocidadY);
-
+    Proyectil(float x, float y, float velocidadX, float velocidadY, int danio = 10);
     virtual ~Proyectil();
 
     void mover() override;
-
     virtual void actualizar() override;
 
     bool estaActivo() const;
-
     void desactivar();
-
-    int getDanio() const;
+    int  getDanio() const;
 };
 
 #endif
