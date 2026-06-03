68f6bc0653a6262749a7f640f1c1d9e2ce0f2d5a
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:25:27 -0500

    Refactor PuntajeJuego class and add error handling
    Refactor PuntajeJuego constructor and add error handling for negative points.

--- logica/PuntajeJuego.cpp
+++ logica/PuntajeJuego.cpp
@@ -1,16 +1,20 @@
 #include "PuntajeJuego.h"
+#include <stdexcept>
 
-PuntajeJuego::PuntajeJuego()
-{
-    puntos = 0;
-}
+PuntajeJuego::PuntajeJuego() : puntos(0), puntosNivelAnterior(0) {}
 
 void PuntajeJuego::agregarPuntos(int cantidad)
 {
+    if (cantidad < 0)
+        throw std::invalid_argument("PuntajeJuego: cantidad negativa no permitida");
     puntos += cantidad;
 }
 
-int PuntajeJuego::getPuntos() const
+void PuntajeJuego::guardarNivel()
 {
-    return puntos;
+    puntosNivelAnterior += puntos;
+    puntos = 0;
 }
+
+int PuntajeJuego::getPuntos()      const { return puntos; }
+int PuntajeJuego::getPuntosTotal() const { return puntos + puntosNivelAnterior; }
