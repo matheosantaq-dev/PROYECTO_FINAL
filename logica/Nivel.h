31237d522eda756ac4d0df0e72cb3f83a227ff48
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:08:44 -0500

    Refactor Nivel.h to include PuntajeJuego and EstadoNivel

--- logica/Nivel.h
+++ logica/Nivel.h
@@ -1,41 +1,48 @@
-
-
 #ifndef NIVEL_H
 #define NIVEL_H
 
 #include <vector>
-
 #include "Jugador.h"
 #include "Enemigo.h"
+#include "PuntajeJuego.h"
 
+// Estado en que puede estar la transicion del nivel
+enum EstadoNivel
+{
+    JUGANDO,
+    TRANSICION, 
+    TERMINADO
+};
+
+// Clase abstracta base para todos los niveles.
 class Nivel
 {
 protected:
-
-    Jugador* jugador;
-
+    Jugador*             jugador;
     std::vector<Enemigo*> enemigos;
-
-    bool terminado;
+    PuntajeJuego         puntaje;
+    EstadoNivel          estado;
+    int                  ticksTransicion;  
 
 public:
-
     Nivel();
-
     virtual ~Nivel();
 
-    virtual void actualizar() = 0;
+    virtual void actualizar()   = 0;
+    virtual void cargarNivel()  = 0;
+    virtual void lanzarBalon()  = 0;
 
-    virtual void cargarNivel() = 0;
+    // Inicia la secuencia de transicion (duracion en ticks)
+    void iniciarTransicion(int duracionTicks = 180);
+    void actualizarTransicion();
 
-    virtual void lanzarBalon() = 0;
+    bool estaTerminado()   const;
+    bool enTransicion()    const;
+    EstadoNivel getEstado() const;
 
-    bool estaTerminado() const;
-
-    Jugador* getJugador() const;
-
-    const std::vector<Enemigo*>&
-    getEnemigos() const;
+    Jugador*                    getJugador()  const;
+    const std::vector<Enemigo*>& getEnemigos() const;
+    int                          getPuntaje()  const;
 };
 
 #endif
