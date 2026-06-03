f5cdbdf81477baad889bfca4045699108e65dd08
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:03:15 -0500

    Refactor Enemigo class member variables and constructor

--- logica/Enemigo.h
+++ logica/Enemigo.h
@@ -3,33 +3,31 @@
 
 #include "Entidad.h"
 
+// Enemigo base: animal de la selva de Jumanji.
+// Hereda de Entidad (herencia propia requerida).
 class Enemigo : public Entidad
 {
 protected:
-
-    int vida;
-
+    int   vida;
     float velocidadMovimiento;
-
-    bool vivo;
-
-    bool moviendoDerecha;
+    bool  vivo;
+    bool  moviendoDerecha;
+    float limiteIzq;
+    float limiteDer;
 
 public:
-
-    Enemigo(float x, float y);
-
+    Enemigo(float x, float y,
+            float limIzq = 0.0f,
+            float limDer = 1150.0f);
     virtual ~Enemigo();
 
-    void mover() override;
-
+    void mover()     override;
     virtual void actualizar() override;
 
     void recibirDanio(int cantidad);
 
     bool estaVivo() const;
-
-    int getVida() const;
+    int  getVida()  const;
 };
 
 #endif
