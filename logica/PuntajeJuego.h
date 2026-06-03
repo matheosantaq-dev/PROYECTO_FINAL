cfb8f66a19281a7fde257e8b80e3cf95358f6701
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:11:02 -0500

    Refactor PuntajeJuego class to add new methods

--- logica/PuntajeJuego.h
+++ logica/PuntajeJuego.h
@@ -1,19 +1,19 @@
 #ifndef PUNTAJEJUEGO_H
 #define PUNTAJEJUEGO_H
 
+
 class PuntajeJuego
 {
 private:
-
     int puntos;
+    int puntosNivelAnterior; 
 
 public:
-
     PuntajeJuego();
-
     void agregarPuntos(int cantidad);
-
-    int getPuntos() const;
+    void guardarNivel();      
+    int  getPuntos()      const;
+    int  getPuntosTotal() const;
 };
 
 #endif
