884337578e2822671a745a7126c0728dcbfeaa89
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:27:35 -0500

    Add Obstaculo class and update NivelSelva

--- logica/NivelSelva.h
+++ logica/NivelSelva.h
@@ -2,42 +2,45 @@
 #define NIVELSELVA_H
 
 #include "Nivel.h"
+#include "Enemigo.h" 
+#include <vector>
+#include <cmath>
+
+class Obstaculo : public Entidad
+{
+public:
+    Obstaculo(float x, float y, int w, int h) : Entidad(x, y, w, h) {}
+    void mover() override {}
+    void actualizar() override {}
+};
 
 class NivelSelva : public Nivel
 {
 private:
-
-    // Distancia recorrida
     int distanciaRecorrida;
-
-    // Tiempo nivel
     int tiempoNivel;
-
-    // Puntos
-    int puntos;
-
-    // Enemigos derrotados
     int enemigosDerrotados;
+    float metaX;
+    int ticksInvulnerable;
+
+    std::vector<Obstaculo*> obstaculos;
 
 public:
-
     NivelSelva();
+    ~NivelSelva();
 
+    // Métodos sobreescritos de Nivel.h
     void actualizar() override;
-
     void cargarNivel() override;
+    void lanzarBalon() override;
 
-   
-    // GETTERS
-   
-
+    // Getters necesarios para HUD y lógica de juego
     int getDistancia() const;
-
-    int getTiempo() const;
-
-    int getPuntos() const;
-
+    int getTiempoSegundos() const;
     int getEnemigosDerrotados() const;
+    float getProgresoMeta() const;
+
+    const std::vector<Obstaculo*>& getObstaculos() const;
 };
 
 #endif
