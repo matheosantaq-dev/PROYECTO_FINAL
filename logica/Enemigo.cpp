3a6bc5a1db2e8f481b9274de375252492b91ba02
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-06-03 17:16:22 -0500

    Refactor Enemigo class constructor and methods

--- logica/Enemigo.cpp
+++ logica/Enemigo.cpp
@@ -1,23 +1,17 @@
 #include "Enemigo.h"
+#include <stdexcept>
 
-Enemigo::Enemigo(float x, float y)
-    : Entidad(x, y)
+Enemigo::Enemigo(float x, float y, float limIzq, float limDer)
+    : Entidad(x, y, 64, 64),
+    vida(100),
+    velocidadMovimiento(3.0f),
+    vivo(true), moviendoDerecha(true),
+    limiteIzq(limIzq), limiteDer(limDer)
 {
-    vida = 50;
-
-    velocidadMovimiento = 3;
-
-    vivo = true;
-
-    moviendoDerecha = true;
-
     velocidadX = velocidadMovimiento;
 }
 
-Enemigo::~Enemigo()
-{
-
-}
+Enemigo::~Enemigo() {}
 
 void Enemigo::mover()
 {
@@ -26,40 +20,34 @@ void Enemigo::mover()
 
 void Enemigo::actualizar()
 {
+    if (!vivo) return;
     mover();
 
-    // Movimiento automático
-
-    if(x <= 0)
+    if (x <= limiteIzq)
     {
         moviendoDerecha = true;
         velocidadX = velocidadMovimiento;
     }
-
-    if(x >= 750)
+    if (x >= limiteDer)
     {
         moviendoDerecha = false;
         velocidadX = -velocidadMovimiento;
     }
 }
 
+// ÚNICA DEFINICIÓN DE LA FUNCIÓN
 void Enemigo::recibirDanio(int cantidad)
 {
+    if (cantidad < 0)
+        throw std::invalid_argument("Enemigo::recibirDanio: valor negativo");
+
     vida -= cantidad;
 
-    if(vida <= 0)
-    {
+    if (vida <= 0) {
         vida = 0;
         vivo = false;
     }
 }
 
-bool Enemigo::estaVivo() const
-{
-    return vivo;
-}
-
-int Enemigo::getVida() const
-{
-    return vida;
-}
+bool Enemigo::estaVivo() const { return vivo; }
+int  Enemigo::getVida()  const { return vida; }
